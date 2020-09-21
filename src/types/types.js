301a7c61980a29f19d400cde19733d0f401c53bc
jorgea306 <jorgea306@gmail.com>
2020-09-21 18:51:11 -0300

    v1 CalendarApp

---
+++ src/types/types.js
@@ -0,0 +1,29 @@
+
+export const types = {
+
+    //Modal
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+    //Eventos
+    eventSetActive: '[event] Set Active',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+
+    //Eventos con base de datos
+    eventStartAddNew: '[event] Start add new',
+    eventLoaded: '[event] Event loaded',
+    eventLogout: '[event] Logout event',
+ 
+    //Autentificacion
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
+}
